cb99424c67426ae119c9eaab16eb05a1aa75d533
Lily Autumn <realprismaticyt@gmail.com>
2026-03-27 06:53:57 +1300

    Revert "Merge pull request #1016 from AutumnalModding/blockgame"
    This reverts commit a4127e4b52d0fda6b451068feab3512d7dbdf597, reversing
    changes made to 678786bdd2c7e3bffb035dcbab5892fc6e8dcae7.

--- Content.Server/Arcade/BlockGame/BlockGameArcadeSystem.cs
+++ Content.Server/Arcade/BlockGame/BlockGameArcadeSystem.cs
@@ -4,7 +4,6 @@ using Content.Shared.Advertise.Components;
 using Content.Shared.Arcade;
 using Content.Shared.Power;
 using Robust.Server.GameObjects;
-using Robust.Shared.Random; // Coyote
 
 namespace Content.Server.Arcade.BlockGame;
 
@@ -12,7 +11,6 @@ public sealed class BlockGameArcadeSystem : EntitySystem
 {
     [Dependency] private readonly UserInterfaceSystem _uiSystem = default!;
     [Dependency] private readonly SpeakOnUIClosedSystem _speakOnUIClosed = default!;
-    [Dependency] private readonly IRobustRandom _random = default!; // Coyote
 
     public override void Initialize()
     {
@@ -28,19 +26,6 @@ public sealed class BlockGameArcadeSystem : EntitySystem
             subs.Event<BlockGameMessages.BlockGamePlayerActionMessage>(OnPlayerAction);
         });
     }
-
-    // Coyote start
-    public void OnGameOver(EntityUid uid, BlockGameArcadeComponent? arcade = null, TransformComponent? xform = null, int points = 0)
-    {
-        if (!Resolve(uid, ref arcade, ref xform))
-            return;
-
-        for (int rewards = 0; rewards < points / 1000; rewards++)
-        {
-            EntityManager.SpawnEntity(_random.Pick(arcade.PossibleRewards), xform.Coordinates);
-        }
-    }
-    // Coyote end
 
     public override void Update(float frameTime)
     {
